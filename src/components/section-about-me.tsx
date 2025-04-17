b3099f9de2c01b6b82b59d1ecb7a71af6d77686b
Carlos Rodrigues <carlos.1994hrs@gmail.com>
2025-04-17 02:03:07 -0300

    fix: img responsivity

--- src/components/section-about-me.tsx
+++ src/components/section-about-me.tsx
@@ -21,7 +21,7 @@ export const SectionAboutMe: FC = () => {
       `}
     >
       <img
-        className="rounded-full w-48 mr-10 max-md:mb-8"
+        className="rounded-full w-48 mr-10 max-md:mb-8 max-md:mr-0"
         src="https://media.licdn.com/dms/image/v2/D4D03AQF8N2Zhot2h_g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1673460652756?e=1750291200&v=beta&t=Zr-YOk7vKsJEQqZR7CkgY9KntqjVDV97Xbk2ZFOLUDQ"
         alt=""
       />
